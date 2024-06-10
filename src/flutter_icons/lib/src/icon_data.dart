98935ef9c13d0a57ac3c27fb3213e6fbd1a55e6f
dangtienngoc <dangtienngoc@hotmail.com>
2024-06-10 18:46:37 +0700

    feat: add Icon data template

---
+++ src/flutter_icons/lib/src/icon_data.dart
@@ -0,0 +1,11 @@
+import "package:flutter/widgets.dart";
+
+class {{className}} extends IconData {
+  const {{className}}(int codePoint)
+      : super(
+          codePoint,
+          fontFamily: "{{fontFamily}}",
+          fontPackage: "{{fontPackage}}",
+          matchTextDirection: true,
+        );
+}
